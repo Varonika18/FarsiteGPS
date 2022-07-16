f028715f0481e740056be363bb559a1a197aa3cf
Varonika <var2020p@gmail.com>
2022-07-16 21:21:15 +0530

    Final Commit

--- app/src/main/java/com/latlongbearing/CameraXActivity.java
+++ app/src/main/java/com/latlongbearing/CameraXActivity.java
@@ -1,28 +1,39 @@
 package com.latlongbearing;
 
+
 import static android.graphics.Paint.ANTI_ALIAS_FLAG;
 import static android.graphics.Paint.DITHER_FLAG;
-import static com.latlongbearing.CameraActivity.bitmap;
 
-import android.Manifest;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Location;
-import android.location.LocationManager;
 import android.media.ExifInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -44,30 +55,17 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
 import androidx.camera.core.CameraX;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureConfig;
 import androidx.camera.core.Preview;
 import androidx.camera.core.PreviewConfig;
-import androidx.compose.ui.graphics.Canvas;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -101,11 +99,6 @@ public class CameraXActivity extends AppCompatActivity  implements SensorEventLi
 
     ImageView zoom_out,zoom_in;
     Button btnLock;
-    FusedLocationProviderClient mFusedLocationClient;
-
-
-    TextView latitudeTextView, longitTextView;
-    int PERMISSION_ID = 44;
     Preview preview;
     int right=0,left=0,top=0,bottom=0;
     int max=0,min=0;
@@ -124,7 +117,7 @@ public class CameraXActivity extends AppCompatActivity  implements SensorEventLi
 
     SharedPreferences sharedPreferences;
 
-/*
+
     public Bitmap addWatermark(Bitmap bitmap, String waterMarkText){
 
         Bitmap result = bitmap.copy(bitmap.getConfig(), true);
@@ -134,7 +127,7 @@ public class CameraXActivity extends AppCompatActivity  implements SensorEventLi
         paint.setColor(Color.WHITE);
         paint.setTextSize(10f);
 
-        *//* val textSize = result.width * *//**//*options.textSizeToWidthRatio*//**//*0.04f;
+        /* val textSize = result.width * *//*options.textSizeToWidthRatio*//*0.04f;
         paint.textSize = textSize;
         paint.color = options.textColor
         if (options.shadowColor != null) {
@@ -143,16 +136,16 @@ public class CameraXActivity extends AppCompatActivity  implements SensorEventLi
         if (options.typeface != null) {
             paint.typeface = options.typeface
         }
-        val padding = result.width * options.paddingToWidthRatio*//*
+        val padding = result.width * options.paddingToWidthRatio*/
         PointF coordinates =
-                calculateCoordinates(waterMarkText, paint, *//*canvas.width*//*1000, *//*canvas.height*//*500, *//*padding*//*100);
+                calculateCoordinates(waterMarkText, paint, /*canvas.width*/1000, /*canvas.height*/500, /*padding*/100);
         canvas.drawText("watermarkText", coordinates.x, coordinates.y, paint);
         return  bitmap;
     }
 
     public PointF calculateCoordinates(String watermarkText, Paint paint, int width, int height, float padding){
         int x = (int) (width-padding);
-        int y *//*= (int) (height-padding)*//*;
+        int y /*= (int) (height-padding)*/;
 
         Rect bounds = new Rect();
         paint.getTextBounds(watermarkText, 0, watermarkText.length(), bounds);
@@ -160,7 +153,7 @@ public class CameraXActivity extends AppCompatActivity  implements SensorEventLi
         y= (int) (textHeight + padding);
 
         return new PointF(x, y);
-    }*/
+    }
 
     @SuppressLint("ResourceAsColor")
     @Override
@@ -174,11 +167,8 @@ public class CameraXActivity extends AppCompatActivity  implements SensorEventLi
         }
 
 
-        latitudeTextView = findViewById(R.id.latTextView);
-        longitTextView = findViewById(R.id.lonTextView);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        getLastLocation();
+
         // Storing data into SharedPreferences
         sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
 
@@ -399,88 +389,6 @@ public class CameraXActivity extends AppCompatActivity  implements SensorEventLi
         });
     }
 
-    @SuppressLint("MissingPermission")
-    private void getLastLocation() {
-        if (checkPermissions()) {
-
-            // check if location is enabled
-            if (isLocationEnabled()) {
-
-                // getting last
-                // location from
-                // FusedLocationClient
-                // object
-                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        Location location = task.getResult();
-                        if (location == null) {
-                            requestNewLocationData();
-                        } else {
-                            latitudeTextView.setText(location.getLatitude() + "");
-                            longitTextView.setText(location.getLongitude() + "");
-                        }
-                    }
-                });
-            } else {
-                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        } else {
-            // if permissions aren't available,
-            // request for permissions
-            requestPermissions();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void requestNewLocationData() {
-
-        // Initializing LocationRequest
-        // object with appropriate methods
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(5);
-        mLocationRequest.setFastestInterval(0);
-        mLocationRequest.setNumUpdates(1);
-
-        // setting LocationRequest
-        // on FusedLocationClient
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
-    }
-
-    private LocationCallback mLocationCallback = new LocationCallback() {
-
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            Location mLastLocation = locationResult.getLastLocation();
-            latitudeTextView.setText("Latitude: " + mLastLocation.getLatitude() + "");
-            longitTextView.setText("Longitude: " + mLastLocation.getLongitude() + "");
-        }
-    };
-
-    // method to check for permissions
-    private boolean checkPermissions() {
-        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-
-        // If we want background location
-        // on Android 10.0 and higher,
-        // use:
-        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private void requestPermissions() {
-        ActivityCompat.requestPermissions(this, new String[]{
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
-    }
-    private boolean isLocationEnabled() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
     private void screenBrightness(double newBrightnessValue) {
         /*
          * WindowManager.LayoutParams settings = getWindow().getAttributes();
@@ -621,7 +529,6 @@ public class CameraXActivity extends AppCompatActivity  implements SensorEventLi
                         intent.putExtra("url",file.getAbsolutePath());
                         startActivity(intent);
 
-                       
 
                         /*
                         geoTag(file.getAbsolutePath(), Double.parseDouble(latitude), Double.parseDouble(longitude));
@@ -913,6 +820,9 @@ public class CameraXActivity extends AppCompatActivity  implements SensorEventLi
 
         return new_lat_long;
     }
+
+
+
 
 
 
